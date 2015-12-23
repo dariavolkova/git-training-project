4dd6e34e51d76cbf6965bbb9e2de68ee96d8739e
msawicki <msawicki@NJ-1TKHFZ1-DCAL.sscincorporated.com>
2015-12-23 13:41:59 -0500

    test 1342

--- src/main/java/com/altegra/gittraining/HelloWorld.java
+++ src/main/java/com/altegra/gittraining/HelloWorld.java
@@ -3,6 +3,6 @@ package com.altegra.gittraining;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!!!!  TEST BRANCH 1223  1337");
+		System.out.println("Hello, world!!!!  TEST BRANCH 1223  1337 1341");
 	}
 }
