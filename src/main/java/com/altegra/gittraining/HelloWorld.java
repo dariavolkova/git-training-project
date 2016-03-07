b9ccdb95056691f74d869def21edfdfaf733aff6
czyanggh <changzhong.yang@altegrahealth.com>
2016-03-07 15:17:49 -0500

    FB number - DB:FB description

--- src/main/java/com/altegra/gittraining/HelloWorld.java
+++ src/main/java/com/altegra/gittraining/HelloWorld.java
@@ -3,7 +3,7 @@ package com.altegra.gittraining;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!!!! This is Altegra at Mt Laurel, NJ, USA");
+		System.out.println("Hello, world!!!! This is Altegra at Mt Laurel, NJ 08054, USA");
 		System.out.println("Hello, world!!!! 4th Floor");
 	}
 }
