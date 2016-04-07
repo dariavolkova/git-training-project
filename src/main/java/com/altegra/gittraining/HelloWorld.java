41f01797345d771afa7dde681eeff3d06e8d06e7
Sabino Magrone <sabino.magrone@altegrahealth.com>
2016-04-07 10:17:44 -0400

    Added a line in Sabino branch

--- src/main/java/com/altegra/gittraining/HelloWorld.java
+++ src/main/java/com/altegra/gittraining/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		System.out.println("Hello, world!!!! This is Altegra at Mt Laurel, NJ 08054, USA");
 		System.out.println("Hello, world!!!! We are at 4th Floor");
 		System.out.println("Hello, America. Sabino here.");
+		System.out.println("I am making a change in Sabino branch.");
 	}
 }
